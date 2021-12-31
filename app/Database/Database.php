6b87057a12be63c01752adc2a9da760ea62df15a
Israel R Pinheiro <israel.pinheiro@live.com>
2021-12-31 10:37:42 -0300

    Add constructor and setConnection

--- app/Database/Database.php
+++ app/Database/Database.php
@@ -51,4 +51,26 @@ class Database{
      * @var PDO
      */
     private $connection;
+
+    /**
+     * Defines the table to be used and instantiate the connection
+     * @param string $table
+     */
+    public function __construct($table=null){
+        $this->table = $table;
+        $this->setConnection();
+    }
+
+    /**
+     * Function responsible for creating the database connection
+     */
+    private function setConnection(){
+        try {
+            $this->connection = new PDO(self::DRIVER.":host=".self::HOST.", dbname=".self::DATABASE.", port=".self::PORT."", self::USER, self::PASSWORD);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Throws an exception if anything goes wrong
+        }
+        catch (PDOException $e) {
+            die('ERROR: '.$e->getMessage()); //Not to be used in production
+        }
+    }
 }
