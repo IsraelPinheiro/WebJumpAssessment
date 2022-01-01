d24251384fc647d5ff794bdd66fa63eae8875374
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 16:03:22 -0300

    Add jquery-easing, chart.js and sweetalert as dev dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 window.Popper = require('popper.js').default;
 window.$ = window.jQuery = require('jquery');
 require('bootstrap');
+//JQuery Easing
+require('jquery-easing')
+//Chart.js
+require('chart.js')
+//Sweetalert
+import swal from 'sweetalert';
