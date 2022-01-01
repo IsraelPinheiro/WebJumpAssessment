a1bcf435b7ad7be249ce37bd51b3e5e79b74f7ee
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 16:38:17 -0300

    Add partial for footer and head

---
+++ resources/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>
+                Copyright &copy;2021 | Made by <a href="https://github.com/IsraelPinheiro" class="font-weight-bold">Israel Pinheiro</a> with <i class='fa fa-heart pulse' style="color:#dc3545"></i> for <a href="https://webjump.com.br" class="font-weight-bold">WebJump</a>
+            </span>
+        </div>
+    </div>
+</footer>
---
+++ resources/includes/head.php
@@ -0,0 +1,13 @@
+<?php include "./vendor/autoload.php"?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Candidate evaluation for a developer position at WebJump ">
+    <meta name="author" content="Israel R Pinheiro">
+    <title>WebJump</title>
+    <link rel="icon" type="image/svg+xml" href="./public/images/favicon.svg">
+    <link rel="alternate icon" href="./public/images/favicon.png">
+    <!-- Styles -->
+    <link href="./public/css/app.css" rel="stylesheet">
+</head>
