bd1a8f851c746e5bb9c72ed0b62629b6e6bd40c9
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 16:38:46 -0300

    Add partial for page bottom and top

---
+++ resources/includes/page_bottom.php
@@ -0,0 +1,13 @@
+                </div>
+            </div>
+            <!-- Footer -->
+            <?php include "./resources/includes/footer.php" ?>
+        </div>
+    </div>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <!-- Scripts -->
+    <script src="./public/js/app.js" defer></script>
+</body>
---
+++ resources/includes/page_top.php
@@ -0,0 +1,14 @@
+<?php include "./resources/includes/head.php"?>
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <?php include "./resources/includes/sidebar.php" ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <!-- Topbar -->
+                <?php include "./resources/includes/topbar.php" ?>
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
