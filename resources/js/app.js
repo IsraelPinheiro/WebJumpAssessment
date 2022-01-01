f2c0f24d95131173d570a892afd8b3c5122bb64a
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 16:16:38 -0300

    Add SBAdmin2 (Custom bootstrap)

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap');
 //JQuery Easing
 require('jquery-easing')
+//SB Admin 2
+require('./sb-admin-2')
 //Chart.js
 require('chart.js')
 //Sweetalert
