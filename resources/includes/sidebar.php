fea5b7b6934eb27994c912d87bd34a2e61ddc644
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 16:38:30 -0300

    Add partial for side and top bars

---
+++ resources/includes/sidebar.php
@@ -0,0 +1,75 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-box"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">WebJump</div>
+    </a>
+    <!-- Divider -->
+    <hr class="sidebar-divider pb-1">
+
+    <!-- Nav Item - Home -->
+    <li class="nav-item">
+        <a class="nav-link pt-0" href="{{ route('home') }}">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Home</span>
+        </a>
+    </li>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link pt-0" href="{{ route('home') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider pb-1">
+    <!-- Nav Item - Products -->
+    <li class="nav-item">
+        <a class="nav-link pt-0" href="{{ route('home') }}">
+            <i class="fas fa-fw fa-box"></i>
+            <span>Products</span>
+        </a>
+    </li>
+    <!-- Nav Item - Categories -->
+    <li class="nav-item">
+        <a class="nav-link pt-0" href="{{ route('home') }}">
+            <i class="fas fa-fw fa-list"></i>
+            <span>Categories</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider pb-1">
+    <!-- Nav Item - Auditing -->
+    <li class="nav-item">
+        <a class="nav-link collapsed pt-0" href="#" data-toggle="collapse" data-target="#collapseAuditing" aria-expanded="true" aria-controls="collapseAuditing">
+            <i class="fas fa-fw fa-eye"></i>
+            <span>Auditing</span>
+        </a>
+        <div id="collapseAuditing" class="collapse" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<!-- TODO: Route Link -->">Access Logs</a>
+                <a class="collapse-item" href="<!-- TODO: Route Link -->">Change Logs</a>
+            </div>
+        </div>
+    </li>
+    <!-- Nav Item - Users -->
+    <li class="nav-item">
+        <a class="nav-link pt-0" href="<!-- TODO: Route Link -->">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Users</span>
+        </a>
+    </li>
+
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
