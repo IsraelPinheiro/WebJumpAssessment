1dccd89e156f22b343b787ad8e504263c6412f03
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-02 19:30:52 -0300

    Move app.js to header

--- resources/includes/page_bottom.php
+++ resources/includes/page_bottom.php
@@ -1,13 +1,11 @@
-                </div>
-            </div>
-            <!-- Footer -->
-            <?php include $_SERVER['DOCUMENT_ROOT']."/resources/includes/footer.php" ?>
-        </div>
-    </div>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-    <!-- Scripts -->
-    <script src="/public/js/app.js" defer></script>
+				</div>
+			</div>
+			<!-- Footer -->
+			<?php include $_SERVER['DOCUMENT_ROOT']."/resources/includes/footer.php" ?>
+		</div>
+	</div>
+	<!-- Scroll to Top Button-->
+	<a class="scroll-to-top rounded" href="#page-top">
+		<i class="fas fa-angle-up"></i>
+	</a>
 </body>
