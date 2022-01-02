d1d7b933fdbb70de80c3385ba1320bd27e91c48c
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-02 13:22:40 -0300

    Add About and Help temporary pages

---
+++ pages/options/about.php
@@ -0,0 +1,15 @@
+<div class="modal fade" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">About</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
+			<div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur feugiat varius. Quisque et tincidunt nulla. Ut ultricies velit quis mollis pretium. Nullam volutpat lobortis metus eget sodales. Nullam et odio quis purus finibus tristique quis eu turpis. Sed porta metus non odio laoreet, sed blandit felis porttitor. Mauris non congue turpis. Vivamus iaculis libero quis nibh auctor, et semper augue malesuada.</p>
+                <p>Donec varius nulla et lorem interdum, eget bibendum quam pellentesque. Mauris id elementum elit. Nam tincidunt metus in diam ultricies tempor. Aenean et condimentum augue. Integer quis justo magna. Nunc auctor ipsum a ex egestas, sed ornare magna sollicitudin. Morbi nunc leo, pellentesque quis quam condimentum, mollis vulputate arcu.</p>
+                <p>Integer sodales ipsum vestibulum sollicitudin egestas. Sed purus ligula, tincidunt sit amet consectetur vel, commodo ac dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Etiam imperdiet nec urna in tincidunt. Sed eleifend enim auctor neque maximus malesuada. Donec mattis mollis dolor, a sodales massa convallis nec. Integer sed augue et dui faucibus faucibus. In tincidunt venenatis dolor id porta. Curabitur at nunc at risus maximus euismod vitae non velit. Duis mollis augue vitae nisl egestas tempor. Nam nibh justo, molestie at rutrum ut, lobortis non ipsum.</p>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ pages/options/help.php
@@ -0,0 +1,15 @@
+<div class="modal fade" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Help</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
+			<div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel pretium urna, nec aliquam eros. Nulla at congue nibh. Nulla mattis ante vel nulla scelerisque rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent rutrum suscipit ex a semper. Quisque lacinia et ex nec tempus. Sed non lacus neque. Ut sit amet nisi convallis turpis consectetur semper. Suspendisse vitae turpis a erat viverra suscipit. Integer faucibus placerat ipsum vitae tincidunt. Aenean condimentum feugiat magna, non bibendum metus luctus eget. Vestibulum rutrum justo enim, sed venenatis libero ultrices auctor. Maecenas sodales magna id lorem convallis, ut venenatis nunc elementum. Suspendisse potenti.</p>
+                <p>Quisque placerat diam in sem dictum, sit amet finibus nisi molestie. Fusce cursus vel eros ut efficitur. Praesent nisl mauris, pulvinar eu bibendum quis, semper sed tortor. Maecenas non mauris vestibulum, mattis velit ac, ullamcorper neque. Suspendisse potenti. Nam nibh dolor, vestibulum non est eu, posuere varius eros. Nulla eget velit elit. Mauris urna lacus, varius a lorem sit amet, euismod consectetur enim. Aliquam auctor, metus eu ultricies pretium, dui urna laoreet nisi, sit amet vulputate sem tellus sed lorem. Maecenas luctus convallis elit, eu semper eros consectetur a. Vestibulum iaculis eget ex et posuere. Donec a mi dolor.</p>
+                <p>Nam efficitur tellus mattis tortor efficitur blandit. Aenean posuere, mi sed sollicitudin sodales, leo orci feugiat nisi, vitae vehicula leo neque vel ante. Phasellus molestie eget metus vel tempor. Nunc vel egestas libero. Duis nibh nisi, euismod eu vestibulum ut, placerat vitae nibh. Ut eget leo erat. Integer pharetra viverra tempor. Etiam venenatis molestie turpis, vel varius nibh accumsan non. Duis et magna a diam fringilla dignissim ac non augue. Maecenas suscipit elit non leo bibendum luctus. Nullam ultricies maximus est in hendrerit. Maecenas mattis turpis eu quam pretium, eu fermentum justo commodo. Etiam in mauris vel nulla efficitur fringilla. Sed tincidunt mi ut urna malesuada, id vehicula risus mollis.</p>
+			</div>
+		</div>
+	</div>
+</div>
