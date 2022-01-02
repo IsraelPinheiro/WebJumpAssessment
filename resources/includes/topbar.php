55a669f3c4b42c2d3a3fd7fc273e96d9afcc7dbb
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-02 18:00:51 -0300

    Refactor top bar

--- resources/includes/topbar.php
+++ resources/includes/topbar.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    use App\Controllers\Auth;
+?>
+
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
     <!-- Sidebar Toggle (Topbar) -->
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -43,34 +48,24 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                @if(Auth::user()->avatar)
-                    <img class="img-profile rounded-circle" src="{{Storage::url("/avatars/").Auth::user()->avatar}}">
-                @else
-                    <img class="img-profile rounded-circle" src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}&size=128&background=4e73df&color=fff">
-                @endif
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo strtok(Auth::user()->name, " ") ?></span>
+                <img class="img-profile rounded-circle" src="https://ui-avatars.com/api/?name=<?php echo Auth::user()->name ?>&size=128&background=4e73df&color=fff">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> {{ __('Meu Perfil') }}
+                <a class="dropdown-item btn-options-activities" href="#">
+                    <i class="fas fa-tasks fa-fw mr-2 text-gray-400"></i> My Activities
                 </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> {{ __('Configurações') }}
+                <a class="dropdown-item btn-options-about" href="#">
+                    <i class="fas fa-info-circle fa-fw mr-2 text-gray-400"></i> About
                 </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> {{ __('Minhas Atividades') }}
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i> {{ __('Alterar Senha') }}
+                <a class="dropdown-item btn-options-help" href="#">
+                    <i class="fas fa-question-circle fa-fw mr-2 text-gray-400"></i> Help
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> {{ __('Sair') }}
+                <a class="dropdown-item btn-options-logout" href="#">
+                    <i class="fas fa-sign-out-alt fa-fw mr-2 text-gray-400"></i> Logout
                 </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
             </div>
         </li>
     </ul>
