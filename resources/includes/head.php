3829f79c22e6f27ffaec8cac6ab9a12d486c16ef
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 22:11:28 -0300

    Fix relative locations

--- resources/includes/head.php
+++ resources/includes/head.php
@@ -1,4 +1,4 @@
-<?php include "./vendor/autoload.php"?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php"?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -6,8 +6,8 @@
     <meta name="description" content="Candidate evaluation for a developer position at WebJump ">
     <meta name="author" content="Israel R Pinheiro">
     <title>WebJump</title>
-    <link rel="icon" type="image/svg+xml" href="./public/images/favicon.svg">
-    <link rel="alternate icon" href="./public/images/favicon.png">
+    <link rel="icon" type="image/svg+xml" href="/public/images/favicon.svg">
+    <link rel="alternate icon" href="/public/images/favicon.png">
     <!-- Styles -->
-    <link href="./public/css/app.css" rel="stylesheet">
+    <link href="/public/css/app.css" rel="stylesheet">
 </head>
