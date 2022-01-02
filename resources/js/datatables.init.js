c045d81668850e73d99e6ffdd7eb47e6efc2101e
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-01 23:08:37 -0300

    Remove custom translation

--- resources/js/datatables.init.js
+++ resources/js/datatables.init.js
@@ -1,7 +1,6 @@
 $(function(){
 	//Inicialização DataTables
 	dataTable = $('.datatable').DataTable({
-		//"dom": "<'row mb-2'<'col-12 text-right'B>>" + "<'row'<'col-sm-6'l><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-5'i><'col-sm-7'p>>",
 		"dom":"<'row'<'col-sm-4'l><'col-sm-4'f><'col-sm-4'B>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-5'i><'col-sm-7'p>>",
         buttons: [
 			'copyHtml5',
@@ -9,30 +8,10 @@ $(function(){
 			'csvHtml5',
 			'pdfHtml5'
 		],
-		"lengthMenu": [[15, 25, 50, -1], [15, 25, 50, "Todos"]],
+		"lengthMenu": [[15, 25, 50, -1], [15, 25, 50, "All"]],
 		"pageLength": 15,
 		"language": {
-			"sEmptyTable": "Nenhum registro encontrado",
-			"sInfo": "Exibindo de _START_ à _END_ de _TOTAL_ registros",
-			"sInfoEmpty": "Exibindo 0 até 0 de 0 registros",
-			"sInfoFiltered": "(Filtrados de _MAX_ registros)",
-			"sInfoPostFix": "",
-			"sInfoThousands": ".",
-			"sLengthMenu": "_MENU_  ",//"_MENU_ itens por página",
-			"sLoadingRecords": "Carregando...",
-			"sProcessing": "Processando...",
-			"sZeroRecords": "Nenhum registro encontrado",
-			"sSearch": "Pesquisar",
-			"oPaginate": {
-				"sNext": "Próximo",
-				"sPrevious": "Anterior",
-				"sFirst": "Primeiro",
-				"sLast": "Último"
-			},
-			"oAria": {
-				"sSortAscending": ": Ordenar colunas de forma ascendente",
-				"sSortDescending": ": Ordenar colunas de forma descendente"
-			}
+			"sLengthMenu": "_MENU_",//"_MENU_ itens per page"
 		},
 		responsive: true,
 		pageResize: true,
