80b9e506210ac38edfc107837dae2d91d845f78f
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-03 16:29:33 -0300

    Fix modal header

--- pages/users/edit.php
+++ pages/users/edit.php
@@ -21,12 +21,11 @@
 
 ?>
 
-
 <div class="modal" tabindex="-1">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">New User</h5>
+				<h5 class="modal-title">Edit User <?php echo $user->name ?></h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
@@ -77,7 +76,7 @@
 				</form>
 			</div>
 			<div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            	<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
 				<button type="submit" class="btn btn-primary btn-users-update" data-id="<?php echo $user->id ?>">Save</button>
 			</div>
 	  	</div>
