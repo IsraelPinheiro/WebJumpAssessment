4cdef12cef755108a1d8e8a8481a47c495a31940
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-02 21:11:56 -0300

    Fix trigger class name

--- pages/categories/new.php
+++ pages/categories/new.php
@@ -31,7 +31,7 @@
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-				<button type="submit" class="btn btn-primary btn-product-store">Save</button>
+				<button type="submit" class="btn btn-primary btn-categories-store">Save</button>
 			</div>
 	  	</div>
 	</div>
