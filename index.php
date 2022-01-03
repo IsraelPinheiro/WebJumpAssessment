e3a980b9da25ea353cdc93250332c7996782d3ef
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-03 17:39:51 -0300

    Set Home bg

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
     include $_SERVER['DOCUMENT_ROOT']."/resources/includes/page_top.php"
 ?>
 
-<?php 
-    
-
-?>
+<div id="home_bg" class="flex"></div>
 
 <?php include $_SERVER['DOCUMENT_ROOT']."/resources/includes/page_bottom.php"?>
