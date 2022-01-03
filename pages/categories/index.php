0cb583e2d1500b66b901f206556fbdbc2c602a97
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-02 22:51:04 -0300

    Add data handling

--- pages/categories/index.php
+++ pages/categories/index.php
@@ -1,6 +1,92 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT']."/resources/includes/page_top.php";
     use App\Models\Category;
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        //Pseudo Method is POST
+        if($_POST["_method"] == "POST"){
+            $data = array();
+            parse_str($_POST["_data"], $data);
+            $data = (object)$data;
+            $category = new Category();
+            $category->name = empty($data->name) ? null:$data->name;
+            $category->description = empty($data->description) ? null:$data->description;
+            if($category->save()){
+                $_SESSION["Alert"] = array(
+                    "Title" => "Success!",
+                    "Text" => "Category created successfully",
+                    "Icon" => "success"
+                );
+                return http_response_code(200);
+            }
+            else{
+                $_SESSION["Alert"] = array(
+                    "Title" => "Error!",
+                    "Text" => "Category couldn't be created",
+                    "Icon" => "error"
+                );
+                return http_response_code(406);
+            }
+        }
+        //Pseudo Method is PUT
+        else if($_POST["_method"] == "PUT"){
+            $category = Category::getById($_POST["_id"]);
+            if($category){
+                $data = array();
+                parse_str($_POST["_data"], $data);
+                $data = (object)$data;
+                $category->name = empty($data->name) ? null:$data->name;
+                $category->description = empty($data->description) ? null:$data->description;
+                if($category->save()){
+                    $_SESSION["Alert"] = array(
+                        "Title" => "Success!",
+                        "Text" => "Category updated successfully",
+                        "Icon" => "success"
+                    );
+                    return http_response_code(200);
+                }
+                else{
+                    $_SESSION["Alert"] = array(
+                        "Title" => "Error!",
+                        "Text" => "Category couldn't be updated",
+                        "Icon" => "error"
+                    );
+                    return http_response_code(406);
+                }
+            }
+            else{
+                $_SESSION["Alert"] = array(
+                    "Title" => "Error!",
+                    "Text" => "Category could not be found",
+                    "Icon" => "error"
+                );
+                return http_response_code(404);
+            }
+        }
+        //Pseudo Method is DELETE
+        else if($_POST["_method"] == "DELETE"){
+            $product = Category::getById($_POST["_id"]);
+            if($product){
+                $product->delete();
+                $_SESSION["Alert"] = array(
+                    "Title" => "Success!",
+                    "Text" => "Category deleted successfully",
+                    "Icon" => "success"
+                );
+                return http_response_code(200);
+            }
+            else{
+                $_SESSION["Alert"] = array(
+                    "Title" => "Error!",
+                    "Text" => "Category could not be found",
+                    "Icon" => "Error"
+                );
+                return http_response_code(404);
+            }
+            
+        }
+    }
+
     $categories = Category::getAll();
 ?>
 <!-- Page Heading --> 
@@ -32,7 +118,6 @@
                         echo '<a data-id='.$category->id.' href="#" class="text-decoration-none btn-categories-edit" title="Edit"><i class="fas fa-edit fa-lg pr-1"></i></a>';
                         echo '<a data-id='.$category->id.' href="#" class="text-decoration-none btn-categories-view" title="View"><i class="fas fa-eye fa-lg pr-1"></i></a>';
                         echo '<a data-id='.$category->id.' href="#" class="text-decoration-none btn-categories-delete" title="Delete"><i class="fas fa-trash-alt fa-lg text-danger"></i></a>';
-                    echo '</td>';
                 echo '</tr>';
             }
         ?>
